1306f8653f4d6ec17abe49862aa32b1c5b36d5d8
Lemon <zuxuguang@163.com>
2018-06-22 11:18:49 +0800

    拼音码排序

--- app/src/main/java/org/lemon/shoutscreen/MainActivity.java
+++ app/src/main/java/org/lemon/shoutscreen/MainActivity.java
@@ -2,7 +2,6 @@
 
 package org.lemon.shoutscreen;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ActivityManager;
@@ -10,12 +9,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.Message;
-import android.support.annotation.NonNull;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -25,74 +23,156 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.TextView;
-//import android.support.v7.app
+
 import com.alibaba.fastjson.JSON;
 
 import org.lemon.commons.Application;
 import org.lemon.commons.CoolToast;
+import org.lemon.commons.HanyuPinyinHelper;
 import org.lemon.commons.MqttService;
 import org.lemon.shoutscreen.util.ShoutScreenUtil;
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
-import pub.devrel.easypermissions.EasyPermissions;
+//import android.support.v7.app
 
 /*
  * MainActivity class that loads MainFragment
  */
-public class MainActivity extends Activity  {
+public class MainActivity extends Activity {
     @ViewInject(R.id.main_gridView)
     private GridView main_gridView;
     @ViewInject(R.id.main_textView)
     private TextView main_textView;
+    @ViewInject(R.id.main_page_textView)
+    private TextView main_page_textView;
     private GridAdapter gridAdapter;
     private UpdateUIBroadcastReceiver broadcastReceiver;
     public static final String ACTION_SCREEN = "org.lemon.commons.MqttService";
     private List<Map<String, Object>> returnData = new ArrayList<Map<String, Object>>();
     private List<Map<String, Object>> showData = new ArrayList<Map<String, Object>>();
     private List<Map<String, Object>> nowData = new ArrayList<Map<String, Object>>();
+    private List<Map<String, Object>> finelData = new ArrayList<Map<String, Object>>();
+    private HanyuPinyinHelper hanyuPinyinHelper = new HanyuPinyinHelper();
     private int ct_of_page = 24;
     private int nowPage = 0;
     private int pageCount = 0;
+    private boolean hasUpdate = false;
     // 定义一个变量，来标识是否退出
     private static boolean isExit = false;
-    private CountDownTimer changeUITime = new CountDownTimer(9999999 * 1000, 7 * 1000) {
+//    private CountDownTimer changeUITime = new CountDownTimer(9999999 * 1000, 8 * 1000) {
+//        @Override
+//        public void onTick(long l) {
+//
+//        }
+//
+//        @Override
+//        public void onFinish() {
+////            Log.i("页码", );
+//        }
+//    };
+
+    private CountDownTimer changeUI1 = new CountDownTimer(6 * 1000, 1 * 1000) {
         @Override
         public void onTick(long l) {
-            nowData = new ArrayList<Map<String, Object>>();
+
+        }
+
+        @Override
+        public void onFinish() {
+            main_textView.setText("请以下人员前来取药");
+        }
+    };
+    private MyAsyncTask task = null;
+
+    private boolean running = true;
+
+    public class MyAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            while (running) {
+                try {
+                    // 处理耗时操作。我们这边线程休眠1秒实现秒表的效果
+                    publishProgress();//类似于发消息给主线程，更新UI
+                    Thread.sleep(8000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            // 为主线程 实现我们刷新UI的逻辑
+            super.onProgressUpdate(values);
+            finelView();
+        }
+
+    }
+
+    private void startTask() {
+        stopTask();
+        running = true;
+        task = (MyAsyncTask) new MyAsyncTask().execute();
+    }
+
+    private void stopTask() {
+        if (task != null) {
+            running = false;
+            task.cancel(true);
+            task = null;
+        }
+    }
+
+    private void finelView() {
+        nowData = new ArrayList<Map<String, Object>>();
+        if (hasUpdate && nowPage == 0) {
+            Log.e("修改", ">");
+            showData = new ArrayList<Map<String, Object>>();
+            showData.addAll(finelData);
+            Log.e("update", String.valueOf(showData.size()));
+            updateUI();
+            hasUpdate = false;
+            nowPage = 0;
+            finelView();
+//                startTask();
+//                return;
+        } else {
             if (nowPage < pageCount - 1) {
-//                Log.i("现在页码", String.valueOf(nowPage));
+                Log.i("现在页码", String.valueOf(nowPage));
                 for (int n = nowPage * ct_of_page; n < (nowPage + 1) * ct_of_page; n++) {
 //                    Log.i("现在个数", String.valueOf(n));
                     nowData.add(showData.get(n));
                 }
                 nowPage++;
+                main_page_textView.setText(nowPage + "/" + pageCount);
             } else if (nowPage == pageCount - 1) {
+                Log.i("现在页码1", String.valueOf(nowPage));
                 for (int n = nowPage * ct_of_page; n < showData.size(); n++) {
                     nowData.add(showData.get(n));
                 }
                 nowPage = 0;
+                main_page_textView.setText(pageCount + "/" + pageCount);
             }
             gridAdapter.notifyDataSetChanged();
         }
-
-        @Override
-        public void onFinish() {
-//            Log.i("页码", );
-        }
-    };
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         x.view().inject(this);
-        Application.imei=getDeviceInfo();
+        Application.imei = getDeviceInfo();
         main_textView.setText("请以下人员前来取药");
         Application.speakChinese("系统初始化完毕");
         // 动态注册广播
@@ -105,28 +185,22 @@ public class MainActivity extends Activity  {
         selectPresThread(0);
     }
 
-    private void nameUtil(List shoutList) {
-//        for (Object o:shoutList
-//             ) {
-//            Map ma=(Map)o;
-////            ma.get("patientName");
-//        }
-//        HashMap<String, Object> map;
-//        for (int x = 0; x <62 ; x++) {
-//            map= new HashMap<String, Object>();
-//            map.put("name","朗读者"+x);
-//            returnData.add(map);
-//        }
-    }
 
     private void updateUI() {
         pageCount = showData.size() / ct_of_page;
         if (showData.size() % ct_of_page != 0) {
             pageCount++;
         }
-//        Log.i("页码", String.valueOf(returnData.size()));
-        changeUITime.cancel();
-        changeUITime.start();
+        //按照拼音排序
+        Collections.sort(showData, new Comparator<Map>() {
+            @Override
+            public int compare(Map map1, Map map2) {
+                return String.valueOf(map1.get("py")).compareTo(String.valueOf(map2.get("py")));
+            }
+        });
+
+        changeUI1.cancel();
+        changeUI1.start();
     }
 
     private void selectPresThread(final int what) {
@@ -141,9 +215,13 @@ public class MainActivity extends Activity  {
                     case 0:
                         returnData = (List) msg.obj;
                         for (int x = 0; x < returnData.size(); x++) {
-                            addMap(showData, returnData.get(x));
+                            Map mp = returnData.get(x);
+                            mp.put("py", hanyuPinyinHelper.getFirstALLLetter(String.valueOf(mp.get("patientName"))));
+                            addMap(showData, mp);
                         }
+                        finelData.addAll(showData);
                         updateUI();
+                        startTask();
 //                        nameUtil ( (List) msg.obj );
                         break;
                 }
@@ -251,20 +329,25 @@ public class MainActivity extends Activity  {
             try {
                 String mes = intent.getStringExtra("people");
                 Map map = JSON.parseObject(mes, Map.class);
-                if(null!=map&&null!=map.get("status")) {
+                if (null != map && null != map.get("status")) {
                     if (0 == Integer.parseInt(String.valueOf(map.get("status")))) {
 //               new ChineseToSpeech().speech("请"+map.get("patientName") + "前来取药");
                         main_textView.setText("请 " + map.get("patientName") + " 前来取药");
                         Application.speakChinese("请  " + map.get("patientName") + "  前来取药");
-                        returnData.add(map);
-                        addMap(showData, map);
-                        updateUI();
+                        map.put("py", hanyuPinyinHelper.getFirstALLLetter(String.valueOf(map.get("patientName"))));
+//                        returnData.add(map);
+                        addMap(finelData, map);
+                        Log.e("add", String.valueOf(finelData.size()));
+//                        updateUI();
                     } else {
-                        removeMap(showData, map);
-                        updateUI();
+                        removeMap(finelData, map);
+                        Log.e("remove", String.valueOf(finelData.size()));
+//                        removeMap(showData, map);
+//                        updateUI();
                     }
+                    hasUpdate = true;
                 }
-            }catch (Exception e){
+            } catch (Exception e) {
                 new CoolToast(getBaseContext()).show("接收数据异常");
             }
 
@@ -287,22 +370,23 @@ public class MainActivity extends Activity  {
     }
 
     private void removeMap(List<Map<String, Object>> list, Map map) {
-        int z = 0;
-        for (int x = 0; x < returnData.size(); x++) {
-            if (map.get("patientId").equals(returnData.get(x).get("patientId"))) {
-                if (map.get("presId").equals(returnData.get(x).get("presId"))) {
-                    returnData.remove(x);
-                }
-                z++;
-            }
-        }
-        if (z == 1) {
+//        int z = 0;
+//        for (int x = 0; x < returnData.size(); x++) {
+//            if (map.get("patientId").equals(returnData.get(x).get("patientId"))) {
+//                if (map.get("presId").equals(returnData.get(x).get("presId"))) {
+//                    returnData.remove(x);
+//                }
+//                z++;
+//            }
+//        }
+//        if (z == 1) {
             for (int x = 0; x < list.size(); x++) {
                 if (map.get("patientId").equals(list.get(x).get("patientId"))) {
                     list.remove(x);
+                    break;
                 }
             }
-        }
+//        }
         System.out.println(returnData.size() + ">>" + list.size());
     }
 
@@ -310,9 +394,11 @@ public class MainActivity extends Activity  {
     protected void onDestroy() {
         unregisterReceiver(broadcastReceiver);
         super.onDestroy();
-        changeUITime.cancel();
-        changeUITime.onFinish();
+        stopTask();
+        changeUI1.cancel();
+        changeUI1.onFinish();
     }
+
     @SuppressLint("MissingPermission")
     private String getDeviceInfo() {
         TelephonyManager mTm = (TelephonyManager) this.getSystemService(TELEPHONY_SERVICE);
@@ -330,6 +416,7 @@ public class MainActivity extends Activity  {
         }
         return super.onKeyDown(keyCode, event);
     }
+
     Handler mHandler = new Handler() {
 
         @Override
@@ -338,6 +425,7 @@ public class MainActivity extends Activity  {
             isExit = false;
         }
     };
+
     private void exit() {
         if (!isExit) {
             isExit = true;
